5e5cecbd3fbdd636fb243535bac5e29ba506fc82
Nadeem Ashraf <dev.nadeemashraf06@gmail.com>
2025-05-30 00:11:59 +0530

    update: removed utis/ folder

--- utils/fs.py
+++
@@ -1,9 +0,0 @@
-def read_file(path: str) -> str:
-    """Read a file at a specified path and return it's content."""
-    try:
-        with open(path, "r", encoding="utf-8") as file:
-            return file.read()
-    except FileNotFoundError:
-        raise RuntimeError(f"File not found: {path}")
-    except IOError as e:
-        raise RuntimeError(f"Error reading file {path}: {e}")
